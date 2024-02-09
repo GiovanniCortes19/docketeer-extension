8c4db279a27a0f2ac21de2cf7f49c67ed1f19bee
GiovanniCortes <giocortes19@gmail.com>
2024-02-09 00:01:30 -0400

    import jest-dom matchers and delete unused beforeAll. Install @docker/extension-api-client-types

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,13 +1,11 @@
 import '@testing-library/jest-dom';
-
 import { cleanup } from '@testing-library/react';
 import { expect, afterEach, vi } from 'vitest';
+import matchers from '@testing-library/jest-dom/matchers';
 
-// // clean up after each test case
-// afterEach(() => {
-//   cleanup();
-// });
+expect.extend(matchers);
 
-beforeAll(() => {
-  vi.mock('docker', () => {});
+// clean up after each test case
+afterEach(() => {
+  cleanup();
 });
